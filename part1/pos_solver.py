8744a126dce170d24b4efa857eb040d8c41cd9a6
Ankit Saxena <ankit2saxena@gmail.com>
2017-11-14 02:38:21 -0500

    updated pos_solver.py

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -10,11 +10,19 @@
 #
 ####
 # Put your report here!!
-#In the train method, we have calculated the probabilites of all words as the first word P(S_1) in the sentence (in dictionary: word_initial)
-#The word emission probabilities P(W_i, S_i) are calculated (in dictionary: word_emission)
-#The state transition probabilities P(S_i+1, S_i) are calculated (in dictionary: state_transition) with key as (S_i, S_i+1)
-#The probability of the existence of a word in the train dataset is calculated (in dictionary: word_dict)
-#The probability of the existence of a state in the train dataset is calculated (in dictionary: state_dict)
+'''
+In the train method, we have calculated the prior probabilities of all words and states in the training set.
+The word emission probabilities P(W_i, S_i) are calculated (in dictionary: word_emission).
+The state transition probabilities P(S_i+1, S_i) are calculated (in dictionary: state_transition) with key as (S_i, S_i+1).
+The probability of the existence of a word in the train dataset is calculated (in dictionary: word_dict).
+The probability of the existence of a state in the train dataset is calculated (in dictionary: state_dict).
+The posterior probability P(W | S) = P(W, S) / P(S)
+where P(W, S) = {P(S1) * P(W1 | S1) * P(S2 | S1) * P(W2 | S2) * P(S3 | S2) * P(W3 | S3) ...}
+on taking the log,
+log{P(W | S)} = log{P(W, S)} - log{P(S)}
+log{P(W | S)} = {log P(S1) + log P(W1 | S1) + log P(S2 | S1) + log P(W2 | S2) + log P(S3 | S2) + ...} - log{P(S)}
+
+''' 
 ####
 
 import random
@@ -28,7 +36,6 @@ import numpy as np
 class Solver:
     
     def __init__(self):
-        self.word_initial = {}
         self.word_emission = {}
         self.state_transition = {}
         self.word_dict = {}
@@ -37,8 +44,38 @@ class Solver:
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
     def posterior(self, sentence, label):
+        logB = 10
         
-        return 0
+        N = len(sentence)
+        sum = 0.0
+        state_prev = None
+        
+        for i in range(0, N):
+            word = sentence[i]
+            state = label[i]
+            
+            if i == 0:
+                if word in self.word_dict:
+                    val = self.word_dict[word]
+                else:
+                    val = 0.00000000000000000000000000000001
+                sum += math.log(val, logB)
+            else:
+                if (word, state_prev) in self.word_emission:
+                    val = self.state_transition[(state_prev, state)]
+                else:
+                    val = 0.00000000000000000000000000000001
+                sum += math.log(val, logB)
+            
+            if (word, state) in self.word_emission:
+                val = self.word_emission[(word, state)]
+            else:
+                val = 0.00000000000000000000000000000001
+            sum += math.log(val, logB)
+            
+            state_prev = label[i]
+        
+        return sum
 
     # Do the training!
     #
@@ -54,14 +91,6 @@ class Solver:
                 count = len(words)
                 for i in range(0, count):
                     
-                    #For the first word or state
-                    if i == 0:
-                        #add the word to the word_initial dictionary P(S_1)
-                        if words[i] not in self.word_initial:
-                            self.word_initial[words[i]] = 1
-                        else:
-                            self.word_initial[words[i]] += 1
-
                     #add the word to the word dictionary
                     if words[i] not in self.word_dict:
                         self.word_dict[words[i]] = 1
@@ -105,28 +134,24 @@ class Solver:
         #converting to probability:
         self.word_dict = dict((key, float(value) / sum(self.word_dict.values())) for (key, value) in self.word_dict.items())
         self.state_dict = dict((key, float(value) / sum(self.state_dict.values())) for (key, value) in self.state_dict.items())
-        self.word_initial = dict((key, float(value) / len(data)) for (key, value) in self.word_initial.items())
+        
         self.state_transition = dict((key, float(value) / sum(self.state_transition.values())) for (key, value) in self.state_transition.items())
         self.word_emission = dict((key, float(value) / sum(self.word_emission.values())) for (key, value) in self.word_emission.items())
         
-        '''
+        #To add the probabilities of events that were not encountered in the training dataset
         for word in self.word_dict:
-            if word not in self.word_initial:
-                self.word_initial[word] = 0.0
-
-            for state in self.state_dict:
+            for state1 in self.state_dict:
+                #state_transitions that were not in the train dataset
                 for state2 in self.state_dict:
-                    if (state, state2) not in self.state_transition:
-                        self.state_transition[(state, state2)] = 0.0
+                    if (state1, state2) not in self.state_transition:
+                        self.state_transition[(state1, state2)] = 0.00000000000000000000000000000001
                                               
-                if (word, state) not in self.word_emission:
-                    self.word_emission[(word, state)] = 0.0
-        '''
-                               
+                #emission probabilities that were not in the train dataset
+                if (word, state1) not in self.word_emission:
+                    self.word_emission[(word, state1)] = 0.00000000000000000000000000000001
+        
         #print '\nWORD PROB: \n', self.word_dict
         #print '\nSTATE PROB: \n', self.state_dict
-        #print '\nWORD INITIAL PROB: \n', self.word_initial
-        #print '\nSTATE TRANSITION PROB \n', self.state_transition
         #print '\nWORD EMISSION PROB \n', self.word_emission
         
         pass
@@ -141,7 +166,7 @@ class Solver:
             for state, value in self.state_dict.items():
                 #denominator can be removed for faster computation
                 if (word, state) in self.word_emission:
-                    prob = float(self.word_emission[(word, state)] * value) / float(self.word_dict[word])
+                    prob = float(self.word_emission[(word, state)] * value)
                     if prob > max_prob:
                         max_prob = prob
                         max_prob_arg = state
@@ -158,33 +183,33 @@ class Solver:
 
         for i in range(len(sentence)):
             word = sentence[i]
+            
             for j in range(len(states)):
                 state = states[j]
-
+                
                 if i == 0:
-                    '''
-                    if word in self.word_initial and (word, state) in self.word_emission:
-                        var_el[j, 0] = self.word_initial[word] * self.word_emission[(word, state)]
-                    else:
-                        var_el[j, 0] = 0.0
-                    '''
                     if (word, state) in self.word_emission:
-                        if word in self.word_initial:
-                            var_el[j, 0] = self.word_initial[word] * self.word_emission[(word, state)]
+                        if state in self.state_dict:
+                            var_el[j, 0] = self.state_dict[state] * self.word_emission[(word, state)]
                         else:
-                            var_el[j, 0] = self.word_emission[(word, state)]
+                            #if the current state was never encountered in the training set
+                            var_el[j, 0] = 0.00000000000000000000000000000001
                     else:
-                        var_el[j, 0] = 0.0
+                        #if the current (word, state) pair was never encountered in the training set
+                        var_el[j, 0] = 0.00000000000000000000000000000001
                 else:
                     temp_sum = 0.0
+                    #k is for previous state
                     for k in range(len(states)):
                         state_prev = states[k]
                         if (state_prev, state) in self.state_transition:
                             temp_sum += (var_el[k, i - 1] * self.state_transition[(state_prev, state)])
+                            
                     if (word, state) in self.word_emission:
                         var_el[j, i] = temp_sum * self.word_emission[(word, state)]
                     else:
-                        var_el[j, i] = 0.0
+                        #if the current (word, state) pair was never encountered in the training set
+                        var_el[j, i] = temp_sum * 0.00000000000000000000000000000001
 
             max_prob_arg = np.argmax(var_el[:, i])
             sequence.append(states[max_prob_arg])
@@ -200,32 +225,30 @@ class Solver:
 
         for i in range(len(sentence)):
             word = sentence[i]
-            for j in range(len(states)):
-                state = states[j]
-
-                if i == 0:
-                    '''
-                    if word in self.word_initial and (word, state) in self.word_emission:
-                        var_el[j, 0] = self.word_initial[word] * self.word_emission[(word, state)]
-                    else:
-                        var_el[j, 0] = 0.0
-                    '''
+            
+            if i == 0:
+                for j in range(len(states)):
+                    state = states[j]
+      
                     if (word, state) in self.word_emission:
-                        if word in self.word_initial:
-                            var_el[j, 0] = self.word_initial[word] * self.word_emission[(word, state)]
+                        if state in self.state_dict:
+                            var_el[j, i] = self.state_dict[state] * self.word_emission[(word, state)]
                         else:
-                            var_el[j, 0] = self.word_emission[(word, state)]
+                            #if the current state was never encountered in the training set
+                            var_el[j, i] = 0.00000000000000000000000000000001
                     else:
-                        var_el[j, 0] = 0.0
-                else:
+                        #if the current (word, state) pair was never encountered in the training set
+                        var_el[j, 0] = 0.00000000000000000000000000000001
+            else:
+                for j in range(len(states)):
+                    state = states[j]
+
                     max_prob = 0.0
                     for k in range(len(states)):
                         state_prev = states[k]
-
+    
                         if (state_prev, state) in self.state_transition:
                             prob = (var_el[k, i - 1] * self.state_transition[(state_prev, state)])
-                        else:
-                            prob = 0.0
                             
                         if prob > max_prob:
                             max_prob = prob
@@ -233,7 +256,8 @@ class Solver:
                     if (word, state) in self.word_emission:
                         var_el[j, i] = max_prob * self.word_emission[(word, state)]
                     else:
-                        var_el[j, i] = 0.0
+                        #if the current (word, state) pair was never encountered in the training set
+                        var_el[j, i] = max_prob * 0.00000000000000000000000000000001
 
             max_prob_arg = np.argmax(var_el[:, i])
             sequence.append(states[max_prob_arg])
